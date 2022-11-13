4a899b24f9c260c092a5b0e8e9147c14cbdd4ed9
POMXARK <1997pom@gmail.com>
2022-11-14 09:29:22 +1000

    repdace JoinStringConverter to StringFormat, deleted LookupItem

--- FriendOrganizer.UI/ViewModels/MainWindowViewModel.cs
+++ FriendOrganizer.UI/ViewModels/MainWindowViewModel.cs
@@ -4,23 +4,14 @@ using FriendOrganizer.UI.Data;
 using ReactiveUI.Fody.Helpers;
 using System.Collections.ObjectModel;
 using System;
-using System.Diagnostics;
 using System.Reactive.Linq;
 using ReactiveUI;
-using System;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using DynamicData.Binding;
 
 
 namespace FriendOrganizer.UI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-
-        public string Greeting => "Welcome to FriendOrganizer!";
 
         private IFriendDataService _friendDataService;
 
@@ -31,23 +22,7 @@ namespace FriendOrganizer.UI.ViewModels
                 .Connect()
                 .Bind(out _friends)
                 .Subscribe();
-
-            _friendDataService = friendDataService;
-            CreateItemFriendList(friendDataService)
-                .Connect()
-                .Transform(x => new LookupItem { DisplayMember = x.FirstName + " " + x.LastName })
-                .Bind(out _Itemfriends)
-                .Subscribe();
         }
-
-        private readonly ReadOnlyObservableCollection<Friend> _friends;
-
-        public ReadOnlyObservableCollection<Friend> Friends => _friends;
-
-
-        private readonly ReadOnlyObservableCollection<LookupItem> _Itemfriends;
-
-        public ReadOnlyObservableCollection<LookupItem> ItemFriends => _Itemfriends;
 
         private ISourceList<Friend> CreateFriendList(IFriendDataService friendDataService)
         {
@@ -56,15 +31,12 @@ namespace FriendOrganizer.UI.ViewModels
             return friends;
         }
 
-        private ISourceList<Friend> CreateItemFriendList(IFriendDataService friendDataService)
-        {
-            var friends = new SourceList<Friend>();
-            friends.AddRange(friendDataService.GetItems());
-            return friends;
-        }
-
         // виртуальное поле для связывания
         [Reactive] Friend SelectedFriend { get; set; }
+
+
+        private readonly ReadOnlyObservableCollection<Friend> _friends;
+        public ReadOnlyObservableCollection<Friend> Friends => _friends;
 
     }
 }
