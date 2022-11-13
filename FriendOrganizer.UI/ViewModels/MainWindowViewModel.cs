c79eb1109e1b39cfc0d078564a73a7460956055c
POMXARK <1997pom@gmail.com>
2022-11-13 12:44:49 +1000

    Add project files.

---
+++ FriendOrganizer.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FriendOrganizer.UI.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ FriendOrganizer.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace FriendOrganizer.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
